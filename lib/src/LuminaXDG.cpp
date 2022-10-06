9a61fd0859c8d460018b4267e9a1b86a1aa27564
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2022-10-06 20:39:39 +0300

    Update LuminaXDG.cpp

--- lib/src/LuminaXDG.cpp
+++ lib/src/LuminaXDG.cpp
@@ -418,7 +418,7 @@ bool XDGDesktop::setAutoStarted(bool autostart){
   if( !filePath.startsWith(upath) && autostart){
     //Some other non-override autostart file - set it up to open with lumina-open
     if(!filePath.endsWith(".desktop")){
-      exec = "lumina-open \""+filePath+"\"";
+      exec = "lumina-open "+filePath;
       tryexec = filePath; //make sure this file exists
       if(name.isEmpty()){ name = filePath.section("/",-1); }
       if(icon.isEmpty()){ icon = LXDG::findAppMimeForFile(filePath); icon.replace("/","-"); }
@@ -427,7 +427,7 @@ bool XDGDesktop::setAutoStarted(bool autostart){
     }else{
       //Some other *.desktop file on the system (keep almost all the existing settings/values)
       // - setup a redirect to the other file
-      exec = "lumina-open \""+filePath+"\"";
+      exec = "lumina-open "+filePath;
       tryexec = filePath; //make sure this file exists
       // - Adjust the actual path where this file will get saved
       filePath = upath+filePath.section("/",-1);
@@ -639,104 +639,6 @@ void XDGDesktopList::populateMenu(QMenu *topmenu, bool byCategory){
   }
 }
 
-//==== LFileInfo Functions ====
-//Need some extra information not usually available by a QFileInfo
-/*void LFileInfo::loadExtraInfo(){
-  desk = 0;
-  //Now load the extra information
-  if( this->suffix().isEmpty() && (this->absoluteFilePath().startsWith("/net/") || this->isDir()) ){
-    mime = "inode/directory";
-    //Special directory icons
-    QString name = this->fileName().toLower();
-    if(name=="desktop"){ icon = "user-desktop"; }
-    else if(name=="tmp"){ icon = "folder-temp"; }
-    else if(name=="video" || name=="videos"){ icon = "folder-video"; }
-    else if(name=="music" || name=="audio"){ icon = "folder-sound"; }
-    else if(name=="projects" || name=="devel"){ icon = "folder-development"; }
-    else if(name=="notes"){ icon = "folder-txt"; }
-    else if(name=="downloads"){ icon = "folder-downloads"; }
-    else if(name=="documents"){ icon = "folder-documents"; }
-    else if(name=="images" || name=="pictures"){ icon = "folder-image"; }
-    else if(this->absoluteFilePath().startsWith("/net/")){ icon = "folder-shared"; }
-    else if( !this->isReadable() ){ icon = "folder-locked"; }
-  }else if( this->suffix()=="desktop"){
-    mime = "application/x-desktop";
-    icon = "application-x-desktop"; //default value
-    desk = new XDGDesktop(this->absoluteFilePath(), 0);
-    if(desk->type!=XDGDesktop::BAD){
-      //use the specific desktop file info (if possible)
-      if(!desk->icon.isEmpty()){ icon = desk->icon; }
-    }
-  }else{
-    //Generic file, just determine the mimetype
-    mime = LXDG::findAppMimeForFile(this->fileName());
-  }
-}
-LFileInfo::LFileInfo(){
-  desk = 0;
-}
-LFileInfo::LFileInfo(QString filepath){ //overloaded contructor
-  this->setFile(filepath);
-  loadExtraInfo();
-}
-LFileInfo::LFileInfo(QFileInfo info){ //overloaded contructor
-  this->swap(info); //use the given QFileInfo without re-loading it
-  loadExtraInfo();
-}
-
-//Functions for accessing the extra information
-// -- Return the mimetype for the file
-QString LFileInfo::mimetype(){
-  if(mime=="inode/directory"){ return ""; }
-  else{ return mime; }
-}
-
-// -- Return the icon to use for this file
-QString LFileInfo::iconfile(){
-  if(!icon.isEmpty()){
-    return icon;
-  }else{
-    if(!mime.isEmpty()){
-      QString tmp = mime;
-      tmp.replace("/","-");
-      return tmp;
-    }else if(this->isExecutable()){
-      return "application-x-executable";
-    }
-  }
-  return ""; //Fall back to nothing
-}
-
-// -- Check if this is an XDG desktop file
-bool LFileInfo::isDesktopFile(){
-  if(desk==0){ return false; }
-  return (!desk->filePath.isEmpty());
-}
-
-// -- Allow access to the XDG desktop data structure
-XDGDesktop* LFileInfo::XDG(){
-  return desk;
-}
-
-// -- Check if this is a readable video file (for thumbnail support)
-bool LFileInfo::isVideo(){
-  if(!mime.startsWith("video/")){ return false; }
-  //Check the hardcoded list of known supported video formats to see if the thumbnail can be generated
-  return ( !LUtils::videoExtensions().filter(this->suffix().toLower()).isEmpty() );
-}
-
-// -- Check if this is a readable image file
-bool LFileInfo::isImage(){
-  if(!mime.startsWith("image/")){ return false; } //quick return for non-image files
-  //Check the Qt subsystems to see if this image file can be read
-  return ( !LUtils::imageExtensions().filter(this->suffix().toLower()).isEmpty() );
-}
-
-bool LFileInfo::isAVFile(){
-  return (mime.startsWith("audio/") || mime.startsWith("video/") );
-}*/
-
-
 //==== LXDG Functions ====
 bool LXDG::checkExec(QString exec){
   //Return true(good) or false(bad)
@@ -853,8 +755,6 @@ void LXDG::setEnvironmentVars(){
   setenv("XDG_DATA_DIRS","/usr/local/share:/usr/share", 0);
   setenv("XDG_CONFIG_DIRS","/etc/xdg:/usr/local/etc/xdg", 0);
   setenv("XDG_CACHE_HOME",QString(QDir::homePath()+"/.cache").toUtf8(), 0);
-  setenv("QT_QPA_PLATFORMTHEME", "lthemeengine", 0);
-  setenv("QT_NO_GLIB","1",0);
   //Don't set "XDG_RUNTIME_DIR" yet - need to look into the specs
 }
 
